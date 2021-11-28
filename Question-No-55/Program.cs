2ffde58ac18b07044e45837b7d02a202958687e6
AqsaAshfaq07 <aqsarao077@gmail.com>
2021-11-28 12:56:33 -0800

    first commit

---
+++ Question-No-55/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 10, b = 20, c = 15, d = 8, e = 40;
+
+//(1)
+Console.WriteLine((a + b / (c - 5)) / ((d + 7) / (e - 37) % 3));
+
+//(2)
+Console.WriteLine(a + b / c - 5 / d + 7 / e - 37 % 3);
+
+//(3)
+Console.WriteLine(a * (b * b) - (c * b) + d);
